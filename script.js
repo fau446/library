44016bf2eea8a0491ab9a4b71d508524a134a18e
Franklin <Franklinau2@gmail.com>
2022-10-11 20:26:23 -0700

    Add function to display books on webpage

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 let myLibrary = [];
 
 function Book(name, author, isRead) {
-  // the constructor...
   this.name = name
   this.author = author
   this.isRead = isRead
 }
 
 function addBookToLibrary(name, author, isRead) {
-  // do stuff here
   const newBook = new Book(name, author, isRead)
   myLibrary.push(newBook)
 }
+
+function displayBooks() {
+  let table = document.querySelector(".books")
+  for (let i = 0; i < myLibrary.length; i++) {
+    let row = table.insertRow(i)
+    let nameCell = row.insertCell(0)
+    let authorCell = row.insertCell(1)
+    let isReadCell = row.insertCell(2)
+
+    nameCell.innerHTML = myLibrary[i].name
+    authorCell.innerHTML = myLibrary[i].author
+    isReadCell.innerHTML = myLibrary[i].isRead
+  }
+}
+
+addBookToLibrary('Bob', 'Tyrants', false)
+addBookToLibrary('Jeebus Heebus', 'Joe', true)
+displayBooks()
